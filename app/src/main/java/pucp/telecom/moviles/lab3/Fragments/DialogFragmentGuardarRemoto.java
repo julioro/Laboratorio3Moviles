d2883a288871cd4c1fce583c1f06864e621b4003
julioro <julio.rodriguezo@pucp.pe>
2020-06-27 10:57:27 -0500

    Fragmento: Guardar remoto

--- app/src/main/java/pucp/telecom/moviles/lab3/Fragments/DialogFragmentGuardarRemoto.java
+++ app/src/main/java/pucp/telecom/moviles/lab3/Fragments/DialogFragmentGuardarRemoto.java
@@ -1,4 +1,31 @@
 package pucp.telecom.moviles.lab3.Fragments;
 
-public class DialogFragmentGuardarRemoto {
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+public class DialogFragmentGuardarRemoto extends androidx.fragment.app.DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getActivity());
+        builder.setMessage("¿Guardar información en la nube?")
+                .setPositiveButton("Guardar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+        return builder.create();
+
+    }
 }
