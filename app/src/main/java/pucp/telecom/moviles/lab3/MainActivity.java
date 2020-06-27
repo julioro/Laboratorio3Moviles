6600ce0ff08c68997e9843324515b6acc5dc79df
julioro <julio.rodriguezo@pucp.pe>
2020-06-27 11:19:14 -0500

    Guardar localmente

--- app/src/main/java/pucp/telecom/moviles/lab3/MainActivity.java
+++ app/src/main/java/pucp/telecom/moviles/lab3/MainActivity.java
@@ -2,7 +2,17 @@ package pucp.telecom.moviles.lab3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void guardarLocal(Medicion m) {
+        // medición_20062020_0225.json
+
+        SimpleDateFormat parser = new SimpleDateFormat("ddMMyyy_HHmm");
+        String fileName = "medicion_" + parser.format(new Date()) + ".json";
+
+        try (FileOutputStream fileOutputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+             FileWriter fileWriter = new FileWriter(fileOutputStream.getFD());) {
+            Gson gson = new Gson();
+            String listaMediciones = gson.toJson(m);
+            fileWriter.write(m);
+
+            Log.d("infoApp", "Guardado exitoso");
+        } catch (IOException e) {
+            Log.d("infoApp", "Error al guardar");
+            e.printStackTrace();
+        }
+
+
     }
 }
